180df62a19257916f1f569840e91342febf6593e
Zachary Youngdale <zyoungdale@gmail.com>
2022-11-23 15:05:29 -0600

    Preview cards render based upon chosen city

--- client/src/CoffeeShopsList.js
+++ client/src/CoffeeShopsList.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import CoffeeShopPreviewCard from "./CoffeeShopPreviewCard";
 
-function CoffeeShopsList({ cities }) {
+function CoffeeShopsList({ cities, coffeeShops }) {
   const [selectedCity, setSelectedCity] = useState("");
 
   let cityOption = cities.map((city) => {
@@ -12,12 +13,31 @@ function CoffeeShopsList({ cities }) {
   });
 
   console.log(selectedCity);
+
+  const filteredCoffeeShopArray = coffeeShops.filter((shop) => {
+    if (shop.city.id === selectedCity) {
+      return shop;
+    }
+  });
+
+  const coffeeShopPreviewCard = filteredCoffeeShopArray.map((shop) => {
+    return <CoffeeShopPreviewCard {...shop} key={shop.id} />;
+  });
   return (
     <div>
-      <select onChange={(e) => setSelectedCity(e.target.value)}>
-        <option>Select Your City</option>
-        {cityOption}
-      </select>
+      <h1 className="flex justify-center m-10 font-san font-black text-4xl">
+        Select Your City
+      </h1>
+      <div className="flex justify-center mb-20 font-sans text-xl">
+        <select
+          onChange={(e) => setSelectedCity(parseInt(e.target.value))}
+          className="p-3"
+        >
+          <option>...</option>
+          {cityOption}
+        </select>
+      </div>
+      <div className="grid grid-cols-2">{coffeeShopPreviewCard}</div>
     </div>
   );
 }
