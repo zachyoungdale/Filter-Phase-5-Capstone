4fdeca6edfd74d1b8edf93d709c851235b04b810
Zachary Youngdale <zyoungdale@gmail.com>
2022-11-23 16:57:09 -0600

    preview card now links to full shop card

--- client/src/CoffeeShopPreviewCard.js
+++ client/src/CoffeeShopPreviewCard.js
@@ -1,8 +1,17 @@
-function CoffeeShopPreviewCard({ name, address }) {
+import { useNavigate } from "react-router-dom";
+
+function CoffeeShopPreviewCard({ name, address, id }) {
+  const navigate = useNavigate();
   return (
     <div className="bg-black text-white font-sans p-6 m-6 flexitems-center rounded-xl">
       <h3 className="font sans text-3xl font-bold">{name}</h3>
       <h4>{address}</h4>
+      <button
+        onClick={() => navigate(`/shop/${id}`)}
+        className="bg-white text-black font-sans font-bold text-xl p-2 rounded-xl mt-3"
+      >
+        See More
+      </button>
     </div>
   );
 }
