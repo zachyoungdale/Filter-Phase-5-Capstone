da51d34910fc4f5cc9a166325edef22c3541c41a
Zachary Youngdale <zyoungdale@gmail.com>
2022-12-07 16:29:00 -0500

    user is actually admin now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password, :password_confirmation, :name, :image)
+        params.permit(:username, :password, :password_confirmation, :name, :image, :admin)
     end
 
     def user_update_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ CoffeeShop.destroy_all
 
 puts "seeding admin user"
 
-User.create(name: "Zachary Youngdale", username: "zachyoungdale", password: "doogie13", password_confirmation: "doogie13")
+User.create(name: "Zachary Youngdale", username: "zachyoungdale", password: "doogie13", password_confirmation: "doogie13", admin: true)
 
 
 
